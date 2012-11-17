67be2bf5f76c6677c324cd457d36bf5ce436d612
Yorkie <l900422@gmail.com>
2012-11-18 03:24:53 +0800

    add some comments
    add some comments

--- dampingScrollbar.js
+++ dampingScrollbar.js
@@ -1,11 +1,12 @@
 // 直接添加到html中就可以看到效果
 
-var speed = 0;		// 速度
-var max__ = 48;		// 最大速度
-var direction = 0;	// 方向
+var speed = 0;		// 速度(不可改)
+var s____ = 6;		// 速度(可配置)
+var max__ = 48;		// 最大速度：最好是s的倍数(可配置)
+var direction = 0;	// 方向(不可改)
 
 $(document).on("mousewheel", function (e) {
-	e.preventDefault(); // 阻止滚动
+	e.preventDefault(); // 阻止默认滚动
 	var t1, t2 = -1, delta = e.originalEvent.wheelDeltaY || e.originalEvent.wheelDelta; // for IE
     	if (speed === 0) {
        		t1 = setTimeout(function () {
@@ -16,6 +17,7 @@ $(document).on("mousewheel", function (e) {
                         	clearTimeout(t1);
                     	}
          	}, 1);
+		// 缓冲(阻尼实现)
                 t2 = setTimeout(function () {
                     	speed > 0 ? speed-- : speed++;
                     	if (speed !== 0) {
@@ -25,13 +27,14 @@ $(document).on("mousewheel", function (e) {
                     	}
            	}, 1);
   	}
+	// 刷新speed的值，实现加速，转向等
     	if (delta < 0) {
          	if (direction > 0) {
                     	speed = 0;
                     	direction = delta;
                     	return;
                 }
-                if (speed <= max__) speed += 6;
+                if (speed <= max__) speed += s____;
                 else {
                     	speed = max__;
                 }
@@ -41,7 +44,7 @@ $(document).on("mousewheel", function (e) {
                     	direction = delta;
                     	return;
                 }
-                if (speed >= -max__) speed -= 6;
+                if (speed >= -max__) speed -= s____;
                 else {
                     	speed = -max__;
                 }
